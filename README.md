4b0db1ca11f1c924bb0cf8b7e925811d00e08763
Simon Lübeß <simon.luebess@gmail.com>
2022-01-02 19:58:12 +0100

    Improved build scripts

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 msdfgen-beef
 
+Setup:
+1. Run `git submodule update --init`
+2. Run `./build_and_copy.ps1 build all`
+3. Run `./build_c.ps1 build all`
+
 Note when building msdfgen:
 You might want to change the project settings "C/C++->General->Debug Information Format" to "Program Database (/Zi)" to prevent getting a warning when building the beef project
--- build_and_copy.ps1
+++ build_and_copy.ps1
@@ -1,33 +1,69 @@
 # Builds msdfgen and copies the library into the dist-Directory
 
-Write-Output "---- Building Library... ----"
-Write-Output "----        DEBUG        ----"
+$buildtype = $args[0]
+$buildmode = $args[1]
 
-devenv .\msdfgen\Msdfgen.sln /project Msdfgen /rebuild "Debug Library"
+Write-Output "----       MSDF (C++)      ----"
 
-Write-Output "----       RELEASE       ----"
-
-devenv .\msdfgen\Msdfgen.sln /project Msdfgen /rebuild "Release Library"
-
-Write-Output "----    COPYING FILES    ----"
-
-$debugInput = ".\msdfgen\x64\Debug Library\";
-$debugOutput = ".\msdfgen-beef\dist\Debug-Win64\";
-$releaseInput = ".\msdfgen\x64\Release Library\";
-$releaseOutput = ".\msdfgen-beef\dist\Release-Win64\";
-
-if(!(Test-Path $debugOutput))
+if ((!$buildtype) -or (!$buildmode))
 {
-    mkdir -Verbose $debugOutput
+    Write-Output "No build options specified..."
+    Write-Output "Usage: build_and_copy.ps1 [build|rebuild] [debug|release|all]"
 }
-
-if(!(Test-Path $releaseOutput))
+else
 {
-    mkdir -Verbose $releaseOutput
+    $rebuildSwitch = ""
+
+    if (($buildtype -eq "rebuild"))
+    {
+        $rebuildSwitch = "/rebuild"
+    }
+    elseif(($buildtype -eq "build"))
+    {
+        $rebuildSwitch = "/build"
+    }
+
+    Write-Output "----  BUILDING MSDF (C++)  ----"
+
+    if (($buildmode -eq "debug") -or ($buildmode -eq "all"))
+    {
+        Write-Output "----     BUILDING DEBUG    ----"
+
+        devenv .\msdfgen\Msdfgen.sln /project Msdfgen $rebuildSwitch "Debug Library"
+
+        Write-Output "----     COPYING FILES     ----"
+
+        $debugInput = ".\msdfgen\x64\Debug Library\";
+        $debugOutput = ".\msdfgen-beef\dist\Debug-Win64\";
+
+        if(!(Test-Path $debugOutput))
+        {
+            mkdir -Verbose $debugOutput
+        }
+    
+        Copy-Item -Verbose -Path "$($debugInput)\msdfgen.lib" -Destination "$($debugOutput)msdfgen.lib"
+        Copy-Item -Verbose -Path "$($debugInput)\msdfgen.pdb" -Destination "$($debugOutput)msdfgen.pdb"
+    }
+    
+    if (($buildmode -eq "release") -or ($buildmode -eq "all"))
+    {
+        Write-Output "----    BUILDING RELEASE   ----"
+
+        devenv .\msdfgen\Msdfgen.sln /project Msdfgen $rebuildSwitch "Release Library"
+
+        Write-Output "----     COPYING FILES     ----"
+
+        $releaseInput = ".\msdfgen\x64\Release Library\";
+        $releaseOutput = ".\msdfgen-beef\dist\Release-Win64\";
+
+        if(!(Test-Path $releaseOutput))
+        {
+            mkdir -Verbose $releaseOutput
+        }
+
+        Copy-Item -Verbose -Path "$($releaseInput)\msdfgen.lib" -Destination "$($releaseOutput)msdfgen.lib"
+        Copy-Item -Verbose -Path "$($releaseInput)\msdfgen.pdb" -Destination "$($releaseOutput)msdfgen.pdb"
+    }
+
+    Write-Output "---- BUILD MSDF (C++) DONE ----"
 }
-
-Copy-Item -Verbose -Path "$($debugInput)\msdfgen.lib" -Destination "$($debugOutput)msdfgen.lib"
-Copy-Item -Verbose -Path "$($debugInput)\msdfgen.pdb" -Destination "$($debugOutput)msdfgen.pdb"
-
-Copy-Item -Verbose -Path "$($releaseInput)\msdfgen.lib" -Destination "$($releaseOutput)msdfgen.lib"
-Copy-Item -Verbose -Path "$($releaseInput)\msdfgen.pdb" -Destination "$($releaseOutput)msdfgen.pdb"
